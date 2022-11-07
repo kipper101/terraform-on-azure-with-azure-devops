a5d33c4f264e1f47a2e06ad2a14b443144100d0b
Pat Martin <pat.martin@enterpriseit.co.nz>
2022-11-08 11:38:52 +1300

    Fix to include required EIT tags.

--- terraform-manifests/c3-locals.tf
+++ terraform-manifests/c3-locals.tf
@@ -4,8 +4,29 @@ locals {
   environment = var.environment
   resource_name_prefix = "${var.business_divsion}-${var.environment}"
   #name = "${local.owners}-${local.environment}"
+  
+  resource_owner = "pat.martin@enterpriseit.co.nz"
+  resource_purpose = "training"
+  resource_client = "servian"
+
   common_tags = {
     owners = local.owners
     environment = local.environment
+    owner = local.resource_owner
+    purpose = local.resource_purpose
+    client = local.resource_client    
   }
+} 
+
+
+
+# Define Local Values in Terraform
+locals {
+  owners = var.business_division
+  environment = var.environment
+  resource_name_prefix = "${var.business_division}-${var.environment}"
+  #name = "${local.owners}-${local.environment}"
+
+  
+
 } 
